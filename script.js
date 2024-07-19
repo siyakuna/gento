f2fab251a998031f514646cdc0ce9a3c23429829
siyakuna <101914509+siyakuna@users.noreply.github.com>
2024-07-19 19:38:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ document.addEventListener('mousemove', function (e) {
     cursor.style.top = y + "px";
     cursor.style.left = x + "px";
   });
-document.getElementById('bgm').volume = 0.13
-document.getElementById('bgm').pause()
-
+  document.getElementById('bgm').volume = 0.13
+  document.getElementById('bgm').pause()
 
 document.querySelectorAll(".menu-tag").forEach(v => v.addEventListener("change", (e) => {
     document.getElementById('page-sound').currentTime = 0; //連続クリックに対応
@@ -33,7 +32,9 @@ document.querySelectorAll(".menu-tag").forEach(v => v.addEventListener("change",
     }
   }));
   function copyPc(){//ココフォリア駒コピー
-    navigator.clipboard.writeText("");
+    navigator.clipboard.writeText(`
+      {"kind":"character","data":{"name":"蓮吟 玄兎 (れんぎん　げんと)","memo":"","initiative":16,"externalUrl":"https://iachara.com/view/9147049","status":[{"label":"HP","value":12,"max":12},{"label":"MP","value":14,"max":14},{"label":"SAN","value":70,"max":70}],"params":[{"label":"STR","value":"6"},{"label":"CON","value":"14"},{"label":"POW","value":"14"},{"label":"DEX","value":"16"},{"label":"APP","value":"11"},{"label":"SIZ","value":"10"},{"label":"INT","value":"15"},{"label":"EDU","value":"12"}],"iconUrl":"https://storage.ccfolia-cdn.net/users/0NOfcmmsyHQICpldAPvVKZ7r4qp1/files/370437c4928e42d488143d655d1e6077435e14268dfcf3ff2f45fb5b9532a971","faces":[{"iconUrl":"https://storage.ccfolia-cdn.net/users/0NOfcmmsyHQICpldAPvVKZ7r4qp1/files/370437c4928e42d488143d655d1e6077435e14268dfcf3ff2f45fb5b9532a971","label":"でふぉ"},{"iconUrl":"https://storage.ccfolia-cdn.net/users/0NOfcmmsyHQICpldAPvVKZ7r4qp1/files/e3004e83673b99941de0fb51b244a3008cd6c616d8eb0f2cfcf14e33fa8f3ff8","label":"笑顔"},{"iconUrl":"https://storage.ccfolia-cdn.net/users/0NOfcmmsyHQICpldAPvVKZ7r4qp1/files/e4924c83abf6f6d119512431b7a9619ce77524a33bb8b0f1ad741d2c470cc841","label":"おびえ"},{"iconUrl":"https://storage.ccfolia-cdn.net/users/0NOfcmmsyHQICpldAPvVKZ7r4qp1/files/4690b4170d2e0324308fe2489e348638de286d93d73995e124c2ce7fe3d77c8b","label":"ばなな"},{"iconUrl":"https://storage.ccfolia-cdn.net/users/0NOfcmmsyHQICpldAPvVKZ7r4qp1/files/d72d906aaee020dda58dc751765576a6f4f3cb0ec1d350e7dc7e5082a31d2232","label":"でふぉ-パーカー無し"},{"iconUrl":"https://storage.ccfolia-cdn.net/users/0NOfcmmsyHQICpldAPvVKZ7r4qp1/files/ec26a185fc2f82bdfcda65aa169091a0fecdde9c03c7d4267ad4ab0559c3e5fe","label":"笑顔ーパーカー無し"}],"x":-278,"y":37,"angle":0,"width":6,"height":6,"active":true,"secret":false,"invisible":false,"hideStatus":false,"color":"#888888","commands":"1d100<={SAN} 【正気度ロール】\nCCB<=75 【アイデア】\nCCB<=70 【幸運】\nCCB<=60 【知識】\nCCB<=70 【目星】\nCCB<=70 【聞き耳】\nCCB<=60 【図書館】\nCCB<=62 【回避】\nCCB<=50 【こぶし（パンチ）】\nCCB<=30 【スピアガン】\nCCB<=60 【応急手当】\nCCB<=60 【忍び歩き】\nCCB<=60 【精神分析】\nCCB<=80 【水泳】\nCCB<=60 【芸術（カリンバ）】\nCCB<=51 【芸術(工作)】\n1d3-1D4 【ダメージ判定】\n1d4-1D4 【ダメージ判定】\n1d6-1D4 【ダメージ判定】\nCCB<={STR}*5 【STR × 5】\nCCB<={CON}*5 【CON × 5】\nCCB<={POW}*5 【POW × 5】\nCCB<={DEX}*5 【DEX × 5】\nCCB<={APP}*5 【APP × 5】\nCCB<={SIZ}*5 【SIZ × 5】\nCCB<={INT}*5 【INT × 5】\nCCB<={EDU}*5 【EDU × 5】\n","owner":""}}
+      `);
     alert('まだ未完成です。サーセン　立ち絵が全部できたら実装します')
   }
 let skillDate =[//技能値
